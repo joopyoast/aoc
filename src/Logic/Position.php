6685934a3013052e58cede834a87fa1924a3d44f
Joop Lammerts <jlammerts83@gmail.com>
2021-12-02 08:49:09 +0100

    Puzzle Two part two

--- src/Logic/Position.php
+++ src/Logic/Position.php
@@ -9,14 +9,16 @@ final class Position {
 
 	private int $horizontal;
 	private int $depth;
+	private int $aim;
 
 	public static function zero(): Position {
-		return new self(0, 0);
+		return new self(0, 0, 0);
 	}
 
-	public function __construct(int $horizontal, int $depth) {
+	public function __construct(int $horizontal, int $depth, int $aim) {
 		$this->horizontal = $horizontal;
 		$this->depth = $depth;
+		$this->aim = $aim;
 	}
 
 	public function move( Movement $movement ): void {
@@ -25,8 +27,8 @@ final class Position {
 
 	private function moverFor( Movement $movement ): Closure {
 		return $movement->isForward()
-			? fn (int $velocity) => $this->horizontal += $velocity
-			: fn (int $velocity) => $this->depth += $velocity;
+			? fn (int $velocity) => $this->moveHorizontal($velocity)
+			: fn (int $velocity) => $this->moveDepth($velocity);
 	}
 
 	public function getHorizontal(): int {
@@ -35,5 +37,14 @@ final class Position {
 
 	public function getDepth(): int {
 		return $this->depth;
+	}
+
+	private function moveHorizontal(int $velocity): void {
+		$this->horizontal += $velocity;
+		$this->depth += ( $velocity * $this->aim );
+	}
+
+	private function moveDepth(int $velocity): void {
+		$this->aim += $velocity;
 	}
 }
